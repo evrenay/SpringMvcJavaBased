81764b5849ea70c74e7ad6c8d6da344312fd548b
evrenay <evrenay16@gmail.com>
2018-03-22 21:21:07 +0300

    güncellendi

--- src/main/java/com/example/testspring/controller/HomeController.java
+++ src/main/java/com/example/testspring/controller/HomeController.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Locale;
 
 @Controller
@@ -26,4 +30,11 @@ public class HomeController {
 
         return "home";
     }
+
+    @RequestMapping(value = "/giris.ajax")
+   public @ResponseBody String giris (@RequestParam String kullaniciAdi, @RequestParam String sifre, HttpServletRequest request, HttpServletResponse response){
+
+        System.out.println("Gelen Parametreler: Kullanıcı Adı: "+ kullaniciAdi +" Sifre :"+ sifre);
+        return kullaniciAdi;
+   }
 }
